b208f896aa7762722440fe217c3aa9305adc5894
NathanCarr10 <[nathancarr18@gmail.com]>
2024-11-08 14:26:04 +0000

    created server.js file

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,31 +1,56 @@
-import {useState} from "react";
+import { useState } from "react"; //useState is React Hook that allows you to add state to a functional component
 
 // Create component definition
-const Create = ()=>{
-    
-    const [title, setTitle] = useState('');
+const Create = () => {
 
-    const handleSubmit =(e)=>{
+    //useState is React Hook that allows you to add state to a functional component
+    const [title, setTitle] = useState('');
+    const [movieYear, setmovieYear] = useState('');
+    const [moviePoster, setmoviePoster] = useState('');
+    
+
+    //this allows you to handle the submissions
+    const handleSubmit = (e) => {
         e.preventDefault();
         console.log(title);
+        console.log(movieYear);
+        console.log(moviePoster);
     }
-
+    //user enters values for movie title, movie year and movie poster url
     return (
         <div>
-          <h2>This is my Create Component.</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label>Add Movie Title: </label>
-              <input type="text"
-                className="form-control"
-                value={title}
-                onChange={(e) => { setTitle(e.target.value) }}
-              />
-            </div>
-            <input type="submit" value="Add Movie" />
-          </form>
+            <h2>This is my Create Component.</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Add Movie Title: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={title}
+                        onChange={(e) => { setTitle(e.target.value) }}
+                    />
+                    <div className="form-group">
+                        <label>Add Movie Year: </label>
+                        <input type="number"
+                            className="form-control"
+                            value={movieYear}
+                            onChange={(e) => { setmovieYear(e.target.value) }}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Add Movie Poster: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={moviePoster}
+                            onChange={(e) => { setmoviePoster(e.target.value) }}
+                        />
+                    </div>
+
+                </div>
+                <input type="submit" value="Add Movie" />
+            </form>
         </div>
-      );
-    }
+    );
+}
 // Exporting the Create component to be used in other parts of the application
 export default Create;
--- src/components/Read.js
+++ src/components/Read.js
@@ -1,28 +1,28 @@
 // Importing the Movies component from the same directory
 import Movies from "./Movies"
-import { useEffect, useState } from "react";
-import axios from "axios";
-const Read = ()=>{
-const [movies, setMovies] = useState([])
-      useEffect(() => {
-        axios.get('https://jsonblob.com/api/jsonblob/1287718524221775872')
-        .then((response)=>{
-          console.log(response.data);
-          setMovies(response.data.movies);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+import { useEffect, useState } from "react"; //By using this Hook, you tell React that your component needs to do something after render
+import axios from "axios"; //Axios helps developers make HTTP requests from NodeJS or XMLHttpRequests from a browser.
+const Read = () => {
+  const [movies, setMovies] = useState([])// useState is React Hook that allows you to add state to a functional component
+  useEffect(() => {
+    axios.get('https://jsonblob.com/api/jsonblob/1287718524221775872')
+      .then((response) => { // Axios request objects have a `then()` function, which means you can use them with promise chains and async/await.
+        console.log(response.data);
+        setMovies(response.data.movies);
+      })
+      .catch((error) => { // handles errors
+        console.log(error);
       });
+  });
 
-    return (
-        <div>
-          {/* Displaying a heading */}
-           <h3> Hello from read component </h3>
-           {/* Passing the movie data (data array) as a prop (myMovies) to the Movies component */}
-           <Movies myMovies={movies}/> 
-        </div>
-    )
+  return (
+    <div>
+      {/* Displaying a heading */}
+      <h3> Hello from read component </h3>
+      {/* Passing the movie data (data array) as a prop (myMovies) to the Movies component */}
+      <Movies myMovies={movies} />
+    </div>
+  )
 }
 //Exporting the Read component so it can be used in other parts of the application
 export default Read;
