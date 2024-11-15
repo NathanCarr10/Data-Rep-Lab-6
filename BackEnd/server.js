e7361f90f69ac4f44c202d0957bb0cc91d1f6f70
NathanCarr10 <[nathancarr18@gmail.com]>
2024-11-15 15:18:56 +0000

    Lab 7 Exercise 7 completed. adding movie data to database from create page

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -32,33 +32,10 @@ const movieSchema = new mongoose.Schema({
 
 const movieModel = new mongoose.model('myMovies', movieSchema);
 
-//importing movie data from /api/movies.js file
-app.get('/api/movies', (req, res) => {
-  const movies = [
-
-    {
-      "Title": "Avengers: Infinity War (server)",
-      "Year": "2018",
-      "imdbID": "tt4154756",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-    },
-    {
-      "Title": "Captain America: Civil War (server)",
-      "Year": "2016",
-      "imdbID": "tt3498820",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-    },
-    {
-      "Title": "World War Z (server)",
-      "Year": "2013",
-      "imdbID": "tt0816711",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-    }
-  ]
-  res.status(200).json({ movies })
+//importing movie data database file
+app.get('/api/movies', async (req, res) => {
+  const movies = await movieModel.find({});
+  res.status(200).json({movies});
 });
 
 //Added a POST method to the Express server that logs the title, year, and poster URL of the movie object passed from the React app
