6d51681c89f47c9bbb1f7e3891c8723ebd818354
NathanCarr10 <[nathancarr18@gmail.com]>
2024-11-15 14:55:47 +0000

    Lab 7 - Exercise 1 to 6 completed

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -7,55 +7,73 @@ const cors = require('cors');
 app.use(cors()); //This middleware setup allows your frontend app (React) to make API requests to the backend (Express) without encountering CORS-related issues.
 
 //added cors headers to enable cors cross origin requests
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-    //Installed body-parser: To handle POST requests
-    const bodyParser = require('body-parser');
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
+//Installed body-parser: To handle POST requests
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-    //importing movie data from /api/movies.js file
+//code to import mongoose database
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://admin:admin@cluster10.paimy.mongodb.net/DB14'); //adding in link to mongoose server
+
+//Variable to add movue data to Mongoose Database
+const movieSchema = new mongoose.Schema({
+  title: String, 
+  year: String,
+  poster: String,
+});
+
+const movieModel = new mongoose.model('myMovies', movieSchema);
+
+//importing movie data from /api/movies.js file
 app.get('/api/movies', (req, res) => {
-    const movies = [
-        
-            {
-              "Title": "Avengers: Infinity War (server)",
-              "Year": "2018",
-              "imdbID": "tt4154756",
-              "Type": "movie",
-              "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-              "Title": "Captain America: Civil War (server)",
-              "Year": "2016",
-              "imdbID": "tt3498820",
-              "Type": "movie",
-              "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            },
-            {
-              "Title": "World War Z (server)",
-              "Year": "2013",
-              "imdbID": "tt0816711",
-              "Type": "movie",
-              "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-            }
-    ]
-    res.status(200).json({movies})
+  const movies = [
+
+    {
+      "Title": "Avengers: Infinity War (server)",
+      "Year": "2018",
+      "imdbID": "tt4154756",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+    },
+    {
+      "Title": "Captain America: Civil War (server)",
+      "Year": "2016",
+      "imdbID": "tt3498820",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+    },
+    {
+      "Title": "World War Z (server)",
+      "Year": "2013",
+      "imdbID": "tt0816711",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+    }
+  ]
+  res.status(200).json({ movies })
 });
 
 //Added a POST method to the Express server that logs the title, year, and poster URL of the movie object passed from the React app
-app.post('/api/movies', (req,res) =>{
-    console.log(req.body.title);
-    res.send('Movie Added');
+app.post('/api/movies',async (req, res) => {
+  console.log(req.body.title);
+  const {title,year,poster} = req.body;
 
+  //saving new Movie Data to Database
+  const newMovie = new movieModel({title,year,poster});
+  await newMovie.save();//The await operator in JavaScript is used within an async function to pause the execution until a Promise is settled, which means it has been either fulfilled or rejected.
+
+  res.status(201).json({"message": "Movie Added!", Movie:newMovie});
 })
 
 //app.listen() in Express is like telling your app to start listening for visitors on a specific address and port, much like how Node listens for connections
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
